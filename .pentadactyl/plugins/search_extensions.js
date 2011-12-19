bf06c7db4d4cc0d0ecfa928c41b41265813b0907
Shalom Bhooshi <s.bhooshi@gmail.com>
2011-12-19 15:15:40 +0000

    First commit of plugins/search_extensions.js

---
+++ .pentadactyl/plugins/search_extensions.js
@@ -0,0 +1,114 @@
+﻿"use strict";
+
+/*
+ *  Install a wikipedia command
+ */
+
+var INFO = 
+<plugin>
+</plugin>;
+
+// Object Literal
+var Abra =  {
+
+  expandVariable : function (v) {
+    var ret = '';
+    var buffer = dactyl.modules.buffer;
+    
+    switch (v) {
+      case '$S': // case '$SELECTION':
+        ret =  buffer.selection.toString();
+        break;
+
+      case '$T': // case '$TITLE':
+        ret = buffer.title;
+        break;
+
+      case '$D': // case '$DOMAIN':
+      case '$H': // case '$HOST':
+        ret = buffer.documentURI.host;
+        break;
+
+      case '$P': // case '$PATH':
+        ret = buffer.documentURI.path;
+        break;
+
+      case '$U': // case '$URI':
+        ret = buffer.documentURI.spec;
+        break;
+
+      case '$Q': // case '$QUERY_STRING':
+        ret = buffer.documentURI.path.match(/(?:\?)(.*)$/);
+        break;
+
+      case '$M': // case '$MEMORY':
+        return typeof m !== 'undefined' ? m : undefined;
+        break
+
+      default :  
+        ret = undefined;
+        break;
+    }
+
+    return ret;
+  
+  },
+
+  expandVariables : function ( instr ) {
+    return instr.replace(/(\$[\w\d_]+)/g, function(m){
+      return Abra.expandVariable(m);
+    })
+  },
+
+  searchString : function ( args ) {
+    if (args.length == 0) return undefined;
+
+    args = args.length ? args.map(function (arg){ 
+          arg = arg.toString();
+
+          arg = arg.match(/\$/) 
+                ? Abra.expandVariables(arg) // expand it
+                : arg;
+
+          arg = arg.match(/\s/) 
+                ? '"' + arg + '"'  // quote it
+                : arg; 
+
+          return arg;
+        }) : [ window.content.window.getSelection() ];
+
+    return unescape( args.join(" ") );
+  },
+
+}
+
+
+
+// TODO : Get the completion stuff working.
+group.commands.add(
+  ["s"],
+  "Search Extensions",
+  function (args) {
+    var cmd = Abra.searchString(args);
+    dactyl.open(cmd);
+    dactyl.echo(cmd, commandline.FORCE_MULTILINE);
+  },
+  {
+    argCount: '*',
+    literal  : Number.MAX_VALUE,
+    completer: function(context, args) {
+      // return completion.bookmark(context, args, '');
+      return completion.search(context);
+    },
+    options: [
+      {
+        names: [],
+        description: "Search Extensions",
+        //type: CommandOption.NOARG,
+        //completer: completion.bookmark
+      }
+    ]
+  },
+  true
+);
+
