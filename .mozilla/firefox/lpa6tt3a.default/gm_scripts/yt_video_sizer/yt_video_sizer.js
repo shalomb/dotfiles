e3722b3c69ec483e3c1dfec1da6003fe1adbb79e
Shalom Bhooshi <s.bhooshi@gmail.com>
2011-12-28 02:25:48 +0000

    Convert fileformat to unix.

--- .mozilla/firefox/lpa6tt3a.default/gm_scripts/yt_video_sizer/yt_video_sizer.js
+++ .mozilla/firefox/lpa6tt3a.default/gm_scripts/yt_video_sizer/yt_video_sizer.js
@@ -15,16 +15,41 @@ function $(id) {
   return el.wrappedJSObject || el;
 }
 
+var elementInDocument = function(element) {
+    while (element) {
+        if (element == document) {
+            return true;
+        }
+        element = element.parentNode;
+    }
+    return false;
+}
+
+
 window.setTimeout(
   function () {
-    var player              = $("watch-player");
 
-    player.style.marginLeft = (  window.innerWidth >= 960 
-                                  ? (985 - window.innerWidth) / 2 
-                                  : "0"
-                              ) + "px";
-    player.style.width      = (window.innerWidth  - 30) + "px";
-    player.style.height     = (window.innerHeight - 50) + "px";
+    try {
+      var player              = $("watch-player");
+
+      player.style.marginLeft = (  window.innerWidth >= 960 
+                                    ? (985 - window.innerWidth) / 2 
+                                    : "0"
+                                ) + "px";
+      player.style.width      = (window.innerWidth  - 30) + "px";
+      player.style.height     = (window.innerHeight - 50) + "px";
+    } catch (e) {}
+
+    try {
+      var player2              = $("movie_player");
+
+      player2.style.marginLeft = (  window.innerWidth >= 960 
+                                    ? (985 - window.innerWidth) / 2 
+                                    : "0"
+                                ) + "px";
+      player2.style.width      = (window.innerWidth  - 30) + "px";
+      player2.style.height     = (window.innerHeight - 50) + "px";
+    } catch (e) {}
 
   }, 500);
 
@@ -37,26 +62,25 @@ $("watch-panel").insertBefore(
 // scroll the title+video container into view
 $("content-container").scrollIntoView(true);
 			
-var style_fragment = "                                                        \
-  #watch-sidebar {                                                            \
-    margin-top: 1px;                                                          \
-  }                                                                           \
-  #watch-video-container {                                                    \
-    background-color: black !important;                                       \
-    background-image :                                                        \
-      -moz-linear-gradient(center top , black, black) !important;             \
-  }                                                                           \
-  #watch-headline-user-info,                                                  \
-  #watch-headline h1,                                                         \
-  #eow-title-input {                                                          \
-    display: inline !important; overflow : scroll !important;                 \
-  }                                                                           \
-  .watch-wide-mode,                                                           \
-  #watch-player,                                                              \
-  #watch-player {                                                             \
-    padding-left:3px!important;                                               \
-  }                                                                           \
-";
+var style_fragment = "                                            " +
+" #watch-sidebar {                                                " +
+"   margin-top: 1px;                                              " +
+" }                                                               " +
+" #watch-video-container {                                        " +
+"   background-color: black !important;                           " +
+"   background-image :                                            " +
+"     -moz-linear-gradient(center top , black, black) !important; " +
+" }                                                               " +
+" #watch-headline-user-info,                                      " +
+" #watch-headline h1,                                             " +
+" #eow-title-input {                                              " +
+"   display: inline !important; overflow : scroll !important;     " +
+" }                                                               " +
+" .watch-wide-mode,                                               " +
+" #watch-player,                                                  " +
+" #watch-player {                                                 " +
+"   padding-left:3px!important;                                   " +
+" }                                                               " ;
 
 GM_addStyle( style_fragment );
 
