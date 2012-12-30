2e075f3e94bd369df0b7a6887d179790978f4385
Shalom Bhooshi <s.bhooshi@gmail.com>
2012-12-30 02:45:07 +0000

    Fix resolution of ~/Desktop/tips

--- .bin/tips_completion.sh
+++ .bin/tips_completion.sh
@@ -2,25 +2,26 @@
 
 # tips_completion.sh - completion for the `tips' command.
 
-tips_dir="${tips_dir:- ~/Desktop/tips}"
+tips_dir="${tips_dir:-$HOME/Desktop/tips}"
 
 _tips() {
 
-  local cur prev opts
+  local current_word previous_word options
   local compwords=()
   COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
-  opts=""
+  current_word="${COMP_WORDS[COMP_CWORD]}"
+  previous_word="${COMP_WORDS[COMP_CWORD-1]}"
+  options=""
 
   oldpwd="$PWD"
-  cd "$tips_dir"
+  if cd "$tips_dir"; then
 
-  while read f; do 
-    COMPREPLY+=( "$f" );
-  done < <( find * \( -name ".hg" -type d -o -name ".*" \) -prune -o -type f -iname "*$cur*" -printf '%p\n' )
-  
-  cd "$oldpwd";
+    while read f; do 
+      COMPREPLY+=( "$f" );
+    done < <( find * \( -name ".hg" -type d -o -name ".*" \) -prune -o -type f -ipath "*$current_word*" -printf '%p\n' )
+    
+    cd "$oldpwd";
+  fi
   return 0
 }
 
