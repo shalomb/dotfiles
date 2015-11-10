61fcb45df7146d8573d6724ff0a843fd3f01ba48
Shalom Bhooshi <s.bhooshi@gmail.com>
2015-11-10 20:23:03 +0000

    'Add .pentadactyl/plugins/split.js'

---
+++ .pentadactyl/plugins/split.js
@@ -0,0 +1,4 @@
+﻿command -js gbm (
+    function(){
+      var a=window,b=buffer.doc,c=encodeURIComponent,d=a.openWebPanel("Google Bookmark", "https://www.google.com/bookmarks/mark?op=edit&output=popup&bkmk="+c(b.location)+"&title="+c(b.title));
+   })();
