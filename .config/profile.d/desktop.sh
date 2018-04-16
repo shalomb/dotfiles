b4b4ce00c77ce68c6d6913b661e431dcb1629772
Shalom Bhooshi <s.bhooshi@gmail.com>
2018-04-16 16:41:00 +0200

    Move DE variables into dedicated file

--- .config/profile.d/desktop.sh
+++ .config/profile.d/desktop.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
 # required by gnome-control-center > 1:3.28
-XDG_CURRENT_DESKTOP=GNOME;   export XDG_CURRENT_DESKTOP;
+XDG_CURRENT_DESKTOP=GNOME;                export XDG_CURRENT_DESKTOP;
 
+# GTK, QT, etc
+GTK2_RC_FILES=~/.gtkrc-2.0;               export GTK2_RC_FILES;
+GDK_USE_XFT="1";                          export GDK_USE_XFT;
+QT_XFT="true";                            export QT_XFT;
+
+# oo.org
+OOO_FORCE_DESKTOP='gnome';                export OOO_FORCE_DESKTOP;
+SAL_USE_VCLPLUGIN='gnome';                export SAL_USE_VCLPLUGIN;
+
+# Fix for being unable to access the accessibility bus
+NO_AT_BRIDGE=1;                           export NO_AT_BRIDGE
+
+GNOME_DESKTOP_SESSION_ID='profile0';      export GNOME_DESKTOP_SESSION_ID;
 
