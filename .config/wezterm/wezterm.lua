b3a9fcba84b743fcca4bab5844d24da3f4cc2df5
Shalom Bhooshi <s.bhooshi@gmail.com>
2022-12-21 21:08:53 +0100

    wezterm: Commit config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,86 @@
+-- wezterm ls-fonts --list-system
+local wezterm = require 'wezterm'
+return {
+  scrollback_lines = 16384,
+  window_background_opacity = 0.95,
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = 'Batman',
+  colors = {
+    foreground = '#bcbcbc',
+    cursor_bg = '#ff6000',
+  },
+  font = wezterm.font_with_fallback {
+    { family="BitStream Vera Sans Mono",
+      weight="Bold", stretch="Normal", style="Normal"
+      -- weight="Bold",
+      -- weight="Normal",
+    },
+    { family="DejaVu Sans Mono",
+      -- weight="Bold",
+      -- weight="Normal",
+    },
+  },
+  font_size=13,
+  font_rules = {
+    -- normal-intensity-and-italic
+    {
+      intensity = 'Normal',
+      italic = true,
+      font = wezterm.font_with_fallback {
+        italic = true,  -- disable the severe italic slant
+        weight = 'Light'
+      },
+    },
+  },
+  quick_select_patterns = {
+    -- match things that look like sha1 hashes
+    -- (this is actually one of the default patterns)
+    '\\S+',
+  },
+  hyperlink_rules = {
+    -- Linkify things that look like URLs and the host has a TLD name.
+    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+    {
+      regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
+      format = '$0',
+    },
+
+    -- linkify email addresses
+    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+    {
+      regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+      format = 'mailto:$0',
+    },
+
+    -- file:// URI
+    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+    {
+      regex = [[\bfile://\S*\b]],
+      format = '$0',
+    },
+
+    -- Linkify things that look like URLs with numeric addresses as hosts.
+    -- E.g. http://127.0.0.1:8000 for a local development server,
+    -- or http://192.168.1.1 for the web interface of many routers.
+    {
+      regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+      format = '$0',
+    },
+
+    -- Make task numbers clickable
+    -- The first matched regex group is captured in $1.
+    {
+      regex = [[\b[tT](\d+)\b]],
+      format = 'https://example.com/tasks/?t=$1',
+    },
+
+    -- Make username/project paths clickable. This implies paths like the following are for GitHub.
+    -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+    -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+    -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+    {
+      regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+      format = 'https://www.github.com/$1/$3',
+    },
+  },
+}
