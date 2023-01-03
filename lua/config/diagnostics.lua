39b8b6d65a474cc188037e0074b59b2d8a5f8ff3
Shalom Bhooshi <s.bhooshi@gmail.com>
2023-01-03 15:21:45 +0100

    nvim: Tidy diagnostics.lua

--- lua/config/diagnostics.lua
+++ lua/config/diagnostics.lua
@@ -4,12 +4,12 @@ local vim = vim
 
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
   vim.lsp.handlers.hover,
-  {border = 'rounded'}
+  { border = 'rounded' }
 )
 
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
   vim.lsp.handlers.signature_help,
-  {border = 'rounded'}
+  { border = 'rounded' }
 )
 
 vim.diagnostic.config({
@@ -56,7 +56,48 @@ local sign = function(opts)
   })
 end
 
-sign({name = 'DiagnosticSignError', text = '✘'})
-sign({name = 'DiagnosticSignWarn', text = '▲'})
-sign({name = 'DiagnosticSignHint', text = '⚑'})
-sign({name = 'DiagnosticSignInfo', text = ''})
+sign({ name = 'DiagnosticSignError', text = '✘' })
+sign({ name = 'DiagnosticSignWarn', text = '▲' })
+sign({ name = 'DiagnosticSignHint', text = '⚑' })
+sign({ name = 'DiagnosticSignInfo', text = '' })
+
+local luasnip = require("luasnip")
+local augroup = vim.api.nvim_create_augroup("luasnip-expand", { clear = true })
+local autocmd = vim.api.nvim_create_autocmd
+
+-- disable diagnostics in insert mode
+-- https://www.reddit.com/r/neovim/comments/uf35lo/comment/i6s4ai9/
+
+autocmd('ModeChanged', {
+  group = augroup,
+  pattern = { 'n:i', 'n:v', 'i:v' },
+  command = 'lua vim.diagnostic.disable(0)'
+})
+
+autocmd('ModeChanged', {
+  group = augroup,
+  pattern = 'i:n',
+  command = 'lua vim.diagnostic.enable(0)'
+})
+
+-- disable diagnostics in snippet expansion
+-- https://www.reddit.com/r/neovim/comments/ug2s4s/disable_diagnostic_while_expanding_luasnip/
+autocmd("ModeChanged", {
+  group    = augroup,
+  pattern  = "*:s",
+  callback = function()
+    if luasnip.in_snippet() then
+      return vim.diagnostic.disable()
+    end
+  end
+})
+
+autocmd("ModeChanged", {
+  group    = augroup,
+  pattern  = "[is]:n",
+  callback = function()
+    if luasnip.in_snippet() then
+      return vim.diagnostic.enable()
+    end
+  end
+})
