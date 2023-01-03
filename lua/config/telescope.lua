b15b1517aefb4569b65a9e5b9bef2ca41d04e3ab
Shalom Bhooshi <s.bhooshi@gmail.com>
2023-01-03 15:17:19 +0100

    nvim: Tidy up telescope config

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -2,34 +2,44 @@
 
 local vim = vim
 
-local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
+local builtin = require("telescope.builtin")
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
+local sorters = require("telescope.sorters")
+local state   = require("telescope.actions.state")
 
-local actions = require 'telescope.actions'
-local finders = require 'telescope.finders'
-local pickers = require 'telescope.pickers'
-local sorters = require 'telescope.sorters'
-local state = require 'telescope.actions.state'
+-- vim.api.nvim_set_hl(0, 'NormalFloat', { ctermfg = "LightGrey" })
 
-vim.api.nvim_set_hl(0, 'NormalFloat', { ctermfg = "LightGrey" })
+require("telescope").setup({
+  defaults = {
+    layout_config = {
+      vertical = { width = 0.75 }
+      -- other layout configuration here
+    },
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key",
+        ["<Up>"] = actions.cycle_history_prev,
+        ["<Down>"] = actions.cycle_history_next,
+      }
+    }
+    -- other defaults configuration here
+  },
+  -- other configuration values here
+})
 
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fc', builtin.commands, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fm', builtin.marks, {})
-vim.keymap.set('n', '<leader>fm', builtin.marks, {})
-
-local builtins = require('telescope.builtin')
 local arr = {}
-for k,_ in pairs(builtins) do
+for k, _ in pairs(builtin) do
   table.insert(arr, k)
 end
 
 local function show_telescope_commands(results)
   pickers.new(_, {
-    prompt_title = 'Telescope Command Finder',
+    prompt_title = "Telescope Command Finder",
     finder = finders.new_table(results),
     sorter = sorters.fuzzy_with_index_bias(),
     attach_mappings = function(prompt_bufnr, _)
@@ -37,7 +47,7 @@ local function show_telescope_commands(results)
         actions.close(prompt_bufnr)
         local selection = state.get_selected_entry()
         local cmd = 'require("telescope.builtin").' .. selection[1]
-        vim.cmd(':lua ' .. cmd .. '()')
+        vim.cmd(":lua " .. cmd .. "()")
         -- print(vim.inspect(selection[0]))
       end)
       return true
@@ -45,7 +55,6 @@ local function show_telescope_commands(results)
   }):find()
 end
 
-vim.keymap.set('n', '<leader>fn', function()
+vim.keymap.set("n", "<leader>fn", function()
   show_telescope_commands(arr)
 end)
-
