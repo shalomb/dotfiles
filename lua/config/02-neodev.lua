64443de05742e3dfbb933e10862b894443c12fb6
Shalom Bhooshi <s.bhooshi@gmail.com>
2023-01-04 00:39:22 +0100

    nvim: Cleanup harpoon config

--- lua/config/02-neodev.lua
+++ lua/config/02-neodev.lua
@@ -1,6 +1,5 @@
 -- lua
 
-
 -- You can override the default detection using the override function
 -- EXAMPLE: If you want a certain directory to be configured differently, you can override its settings
 require("neodev").setup({
--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -8,32 +8,40 @@ local harpoon_ui = require('harpoon.ui')
 local harpoon_tmux = require('harpoon.tmux')
 local harpoon_mark = require('harpoon.mark')
 
-local map = vim.keymap.set
--- TODO find out how to map apostrophe in lua
-vim.cmd([[
-  map <leader>' :lua require("harpoon.ui").toggle_quick_menu()<cr>
-]])
+require("telescope").load_extension('harpoon')
 
-map("n", "<leader>he", function()
-  harpoon_ui.toggle_quick_menu()
-end)
+local whichkey = require("which-key")
+whichkey.register({
+  ["'"] = { require("harpoon.ui").toggle_quick_menu, "harpoon menu" },
+  h = {
+    name = "harpoon",
+    a = {
+      function()
+        harpoon_mark.add_file()
+        local file = vim.fn.expand('%')
+        vim.fn.OK(file .. ' harpooned.')
+      end, "harpoon file"
+    },
+    m = { function() vim.cmd([[:Telescope harpoon marks]]) end, "term 1" },
+    t = { harpoon_tmux.gotoTerminal, "term 1" },
+  },
+}, { mode = "n", prefix = "<leader>" })
 
-map("n", "<leader>#", function()
-  vim.cmd([[:Telescope harpoon marks]])
-end)
-
-map("n", "<leader>ht", function()
-  harpoon_tmux.gotoTerminal(1)
-end)
-
-map("n", "<leader>ha", function()
-  harpoon_mark.add_file()
-  local file = vim.fn.expand('%')
-  vim.fn.OK(file .. ' harpooned.')
-end)
-
-for i=1,9 do
-  map("n", "<leader>" .. i, function()
+for i = 1, 9 do
+  vim.keymap.set("n", "<leader>" .. i, function()
     harpoon_ui.nav_file(i)
-  end)
+  end, { expr = false, desc = string.format("h'poon %s", i) })
 end
+
+local group = vim.api.nvim_create_augroup("Harpoon Augroup", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "harpoon",
+  group = group,
+  callback = function()
+    vim.keymap.set("n", "<C-V>", function()
+      local harpooned = vim.api.nvim_get_current_line()
+      local pwd = vim.fn.getcwd() .. "/"
+      vim.cmd("vsplit | edit " .. pwd .. harpooned)
+    end, { buffer = true, noremap = true, silent = true })
+  end,
+})
