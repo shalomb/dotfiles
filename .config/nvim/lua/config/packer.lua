b8c9911bb2a5aa2c9dde4dba32bbd9160aad7db8
Shalom Bhooshi <s.bhooshi@gmail.com>
2023-09-07 19:33:26 +0200

    Setup leoluz/nvim-dap-go

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -102,7 +102,7 @@ return packer.startup(function(use)
       "rcarriga/nvim-dap-ui",
       "mfussenegger/nvim-dap-python",
       "nvim-telescope/telescope-dap.nvim",
-      { "leoluz/nvim-dap-go", module = "dap-go" },
+      { "leoluz/nvim-dap-go",                module = "dap-go" },
       { "jbyuki/one-small-step-for-vimkind", module = "osv" },
     },
     config = function()
