f8e84d043af0454211439bf32d4cf25d0531651a
Shalom Bhooshi <s.bhooshi@gmail.com>
2024-10-11 19:00:37 +0200

    nvim: Install kevinhwang91/nvim-ufo

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -190,4 +190,27 @@ return {
   { "kana/vim-textobj-line",          lazy = true,  event = "InsertEnter", },
   { "wellle/targets.vim",             lazy = true,  event = "InsertEnter", }, -- Add mode text objects
 
+  {
+    "kevinhwang91/nvim-ufo",
+    event = "BufRead",
+    dependencies = {
+      { "kevinhwang91/promise-async" },
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            -- foldfunc = "builtin",
+            -- setopt = true,
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
+              { text = { "%s" },                  click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          })
+        end,
+      },
+    },
+  }
 }
