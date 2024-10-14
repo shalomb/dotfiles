def4fefdd1d4078dee0c8d3d45881ffe8d1d4e1e
Shalom Bhooshi <s.bhooshi@gmail.com>
2024-10-14 07:35:42 +0200

    nvim: Add keymaps for dap-ui

--- .config/nvim/lua/config/dap.lua
+++ .config/nvim/lua/config/dap.lua
@@ -157,4 +157,54 @@ whichkey.add({
     desc = "run last",
   },
 
+  {
+    "<leader>duo",
+    function()
+      require("dapui").setup()
+      require("dapui").open()
+    end,
+    desc = "dapui open",
+  },
+
+  {
+    "<leader>duc",
+    function()
+      require("dapui").setup()
+      require("dapui").close()
+    end,
+    desc = "dapui close",
+  },
+
+  {
+    "<leader>duf",
+    function()
+      require("dapui").setup()
+      require("dapui").float_element()
+    end,
+    desc = "dapui float element",
+  },
+
+  {
+    "<leader>de",
+    function()
+      require("dapui").setup()
+      require("dapui").eval()
+    end,
+    desc = "dapui eval expression",
+  },
+
 })
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
