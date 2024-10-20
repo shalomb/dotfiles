373f552e7662250132a5c2915794a816538389e2
Shalom Bhooshi <s.bhooshi@gmail.com>
2024-10-20 12:27:18 +0200

    nvim: Document formatoptions settings better

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,25 +1,33 @@
 -- options
 
-local vim = vim
+local vim           = vim
 
 -- TODO options.wildcharm="<C-Z>"
 
 -- options.errorformat:append('%f|%l col %c|%m')
 
-vim.opt.formatoptions:remove('t')                                         -- text wrap autoindent
-
-vim.opt.completeopt    = "longest,menu,menuone,preview,noinsert,noselect" -- set by lsp-zero
-vim.opt.autoindent     = true
-vim.opt.backup         = false
-vim.opt.cindent        = true
-vim.opt.clipboard      = 'unnamedplus'
-vim.opt.cmdheight      = 1
-vim.opt.complete       = ',w,b,u,t,i'
-vim.opt.cursorline     = true
-vim.opt.encoding       = "UTF-8"
-vim.opt.expandtab      = true
-vim.opt.foldlevel      = 16
-vim.opt.foldmethod     = 'indent'
+vim.opt.autoindent  = true
+vim.opt.backup      = false
+vim.opt.cindent     = true
+vim.opt.clipboard   = 'unnamedplus'
+vim.opt.cmdheight   = 2
+vim.opt.complete    = ',w,b,u,t,i'
+vim.opt.completeopt = "longest,menu,menuone,preview,noinsert,noselect" -- set by lsp-zero
+vim.opt.cursorline  = true
+vim.opt.encoding    = "UTF-8"
+vim.opt.expandtab   = true
+vim.opt.foldlevel   = 16
+vim.opt.foldmethod  = 'indent'
+vim.opt.formatoptions:append('1') -- don't break lines after one-letter words
+vim.opt.formatoptions:append('c') -- auto-wrap comments on textwith
+vim.opt.formatoptions:append('j') -- remove comment leader when joining lines
+vim.opt.formatoptions:append('l') -- long lines are not broken in insert mode
+vim.opt.formatoptions:append('o') -- insert comment leader on 'o/O' in normal mode
+vim.opt.formatoptions:append('q') -- format comments with 'gq'
+vim.opt.formatoptions:append('r') -- insert comment leader after <CR>
+vim.opt.formatoptions:remove('a') -- auto format paragraphs
+vim.opt.formatoptions:remove('n') -- recognize numbered lists
+vim.opt.formatoptions:remove('t') -- remove autoindent on textwidth
 vim.opt.hidden         = true
 vim.opt.hlsearch       = true
 vim.opt.inccommand     = "split"
