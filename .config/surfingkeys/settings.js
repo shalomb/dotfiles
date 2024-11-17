5c7bd5253c350e188c84f8ad219cba52563dcc41
Shalom Bhooshi <s.bhooshi@gmail.com>
2024-11-17 20:09:20 +0100

    surfingkeys: backup settings.js

--- .config/surfingkeys/settings.js
+++ .config/surfingkeys/settings.js
@@ -2,12 +2,12 @@
 
 // unmap e/d scroll half page up/down
 const unmaps = [
-  'e', 'e',
-  '<Ctrl-b>', '<Ctrl-f>'
+    'e', 'e',
+    '<Ctrl-b>', '<Ctrl-f>'
 ];
 
 unmaps.forEach((u) => {
-  api.unmap(u);
+    api.unmap(u);
 });
 
 // remap for a vim-like experience
@@ -21,7 +21,7 @@ api.vmap('<Ctrl-[>', '<Esc>');
 
 // an example to create a new mapping `ctrl-y`
 api.mapkey('<Ctrl-y>', 'Show me the money', function() {
-  api.Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
+    api.Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
 });
 
 // an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
@@ -32,7 +32,7 @@ api.mapkey('S', '#8Open opened URL in current tab', 'Normal.openOmnibar({type: "
 
 api.unmap('ymm');
 api.mapkey('ymm', '#7Copy the current URL as a markdown link to the clipboard', function() {
-  api.Clipboard.write('[' + window.location.hostname + ': ' + document.title + '](' + window.location.href + ')');
+    api.Clipboard.write('[' + window.location.hostname + ': ' + document.title + '](' + window.location.href + ')');
 });
 
 // an example to remove mapkey `Ctrl-i`
@@ -43,25 +43,25 @@ api.cmap('<Ctrl-n>', '<Tab>');
 api.cmap('<Ctrl-p>', '<Shift-Tab>');
 
 api.mapkey('<Space>', 'Choose a tab with omnibar', function() {
-  api.Front.openOmnibar({ type: "Tabs" });
+    api.Front.openOmnibar({ type: "Tabs" });
 });
 
 api.mapkey('ou', '#8Open AWS services', function() {
-  var services = Array.from(top.document.querySelectorAll('#awsc-services-container li[data-service-href]')).map(function(li) {
-    return {
-      title: li.querySelector("span.service-label").textContent,
-      url: li.getAttribute('data-service-href')
-    };
-  });
-  if (services.length === 0) {
-    services = Array.from(top.document.querySelectorAll('div[data-testid="awsc-nav-service-list"] li[data-testid]>a')).map(function(a) {
-      return {
-        title: a.innerText,
-        url: a.href
-      };
+    var services = Array.from(top.document.querySelectorAll('#awsc-services-container li[data-service-href]')).map(function(li) {
+        return {
+            title: li.querySelector("span.service-label").textContent,
+            url: li.getAttribute('data-service-href')
+        };
     });
-  }
-  api.Front.openOmnibar({ type: "UserURLs", extra: services });
+    if (services.length === 0) {
+        services = Array.from(top.document.querySelectorAll('div[data-testid="awsc-nav-service-list"] li[data-testid]>a')).map(function(a) {
+            return {
+                title: a.innerText,
+                url: a.href
+            };
+        });
+    }
+    api.Front.openOmnibar({ type: "UserURLs", extra: services });
 }, { domain: /console.amazonaws|console.aws.amazon.com/i });
 
 settings.lurkingPattern = /.*onetakeda.atlassian.net/i;
@@ -73,10 +73,10 @@ settings.hintAlign = "left";
 // search engines
 
 api.addSearchAlias('d', 'duckduckgo', 'https://duckduckgo.com/?q=', 's', 'https://duckduckgo.com/ac/?q=', function(response) {
-  var res = JSON.parse(response.text);
-  return res.map(function(r) {
-    return r.phrase;
-  });
+    var res = JSON.parse(response.text);
+    return res.map(function(r) {
+        return r.phrase;
+    });
 });
 
 
