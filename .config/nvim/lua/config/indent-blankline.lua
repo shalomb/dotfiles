a02f5a4a82d16f4d5c3389e3399afe40f028f2ca
Shalom Bhooshi <s.bhooshi@gmail.com>
2024-11-29 23:33:38 +0100

    nvim: "increasingly annoying" blanklines to force refactoring

--- .config/nvim/lua/config/indent-blankline.lua
+++ .config/nvim/lua/config/indent-blankline.lua
@@ -1,27 +1,41 @@
 -- indent-blankline
 
 local highlight = {
-    "RainbowGray",
+    "RainbowGray1",
+    "RainbowGray2",
+    "RainbowGray3",
+    "RainbowGray4",
+    "RainbowGray5",
+    "RainbowGray6",
+    "RainbowGray7",
+    "RainbowGray8",
 }
 
 local hooks = require "ibl.hooks"
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowGray", { fg = "#383838" })
+    vim.api.nvim_set_hl(0, "RainbowGray1", { fg = "#383838" })
+    vim.api.nvim_set_hl(0, "RainbowGray2", { fg = "#484848" })
+    vim.api.nvim_set_hl(0, "RainbowGray3", { fg = "#585858" })
+    vim.api.nvim_set_hl(0, "RainbowGray4", { fg = "#787878" })
+    vim.api.nvim_set_hl(0, "RainbowGray5", { fg = "#888888" })
+    vim.api.nvim_set_hl(0, "RainbowGray6", { fg = "#989898" })
+    vim.api.nvim_set_hl(0, "RainbowGray7", { fg = "#a8a8a8" })
+    vim.api.nvim_set_hl(0, "RainbowGray8", { fg = "#f8f8f8" })
 end)
 
 require("ibl").setup {
     indent = {
-      highlight = highlight,
-      char = "·"
+        highlight = highlight,
+        char = "·"
     },
     whitespace = {
         highlight = highlight,
         remove_blankline_trail = false,
     },
     scope = {
-      enabled = false
+        enabled = true
     },
 }
 
--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -15,7 +15,7 @@ end
 local config_dir = script_path()
 
 local function LoadBundle(dir, suffix)
-  local files = vim.fn.glob(config_dir .. '/'.. dir .. '/*.' .. suffix)
+  local files = vim.fn.glob(config_dir .. '/' .. dir .. '/*.' .. suffix)
   local vim_scripts = vim.split(files, '\n')
   for _, file in pairs(vim_scripts) do
     vim.cmd('source ' .. file)
