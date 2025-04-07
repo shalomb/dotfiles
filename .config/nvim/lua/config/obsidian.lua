af30874a7835863d08ddc45664b67004e2599a99
Shalom Bhooshi <s.bhooshi@gmail.com>
2025-04-07 08:32:29 +0200

    obsidian: Move templates to Assets/Templates

--- .config/nvim/lua/config/obsidian.lua
+++ .config/nvim/lua/config/obsidian.lua
@@ -159,7 +159,7 @@ obsidian.setup({
 
   -- Optional, for templates (see below).
   templates = {
-    folder = "templates",
+    folder = "Asset/Templates",
     date_format = "%Y-%m-%d",
     time_format = "%H:%M",
     -- A map for custom variables, the key should be the variable and the value a function
