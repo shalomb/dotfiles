52cbe4a3623cd2a2b4b1b2e83f9b8c5088e37d91
Shalom Bhooshi <s.bhooshi@gmail.com>
2025-07-10 11:49:57 +0200

    workspace: Have ipython be the interactive python shell

--- .config/pyrc.py
+++ .config/pyrc.py
@@ -2,21 +2,26 @@
 
 # .pythonstartup
 
+import atexit
 import json
-import yaml
 import os
-import sys
 import requests
-from icecream import ic
-from pathlib import Path
+import sys
+import time
+import yaml
+import re
 # import stackprinter
 
-from rich import print, print_json, pretty, traceback, inspect
 from datetime import datetime
-import time
 from importlib import reload
 from pprint import pp
+from icecream import ic
+from pathlib import Path
 
+from rich import print, print_json, pretty, traceback, inspect
+from rich.panel import Panel
+from rich.color import Color
+from rich.console import Console
 
 # stackprinter.set_excepthook(style='darkbg2')  # for jupyter notebooks try style='lightbg'
 
@@ -26,13 +31,35 @@ now = datetime.now
 
 # https://realpython.com/python-repl/#colorizing-repl-output-with-rich
 pretty.install()
+console = Console()
+
 traceback.install(show_locals=True)
 
 i = inspect
+p = console.print
+l = console.log
 
 
 def n():
     return now().strftime("%FT%T.%f")
 
-sys.ps1='\x01\x1b[1;49;33m\x02>\x01\x1b[0m\x02 '
-sys.ps2='\x01\x1b[1;49;31m\x02.\x01\x1b[0m\x02 '
+
+def bye():
+    from rich.console import Console
+
+    p = Console().print
+    p()
+    p("Alrighty! Catch you on the bright side!")
+
+
+atexit.register(bye)
+
+
+sys.ps1 = "\x01\x1b[1;49;33m\x02>\x01\x1b[0m\x02 "
+sys.ps2 = "\x01\x1b[1;49;31m\x02.\x01\x1b[0m\x02 "
+
+# i(sys.version)
+p()
+p(os.getcwd())
+p(sys.version)
+p()
